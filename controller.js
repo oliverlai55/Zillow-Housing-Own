15e7ceb9fa14d76f6e0d33a93e70a738c1449ca1
oliverlai55 <oliverlai55@gmail.com>
2015-11-16 11:32:09 -0500

    change index.html

--- controller.js
+++ controller.js
@@ -1,13 +1,6 @@
 var houseCtl = angular.module('myApp', []);
 myApp.controller('mainController', function($scope, $http) {
+	$scope.items = houses;
+
 	
-	$scope.relatedVids = otherVideos;
-	singleVideoUrl = 'https://www.googleapis.com/youtube/v3/search?relatedToVideoId=0a33WTZK2zI&part=snippet&type=video&maxResults=10&key=AIzaSyB2_xXL68_ejdpXl5smjhuY3Sx2D888Zn4';
-	$http.get(singleVideoUrl).success(function(data) {
-         $scope.relatedVids = data.items;
-         console.log(data.items);
-       });
-	// $http.get({
-	// 	url: 'https://www.googleapis.com/youtube/v3/search?relatedToVideoId=0a33WTZK2zI&part=snippet&type=video&key=AIzaSyB2_xXL68_ejdpXl5smjhuY3Sx2D888Zn4'
-	// });
 });
